f20e15c652018b69ca206d62c37ba2d986bd4ed5
ktyntang <ktyntang@hotmail.com>
2022-08-02 15:36:31 +0800

    Fixed link syntax in save_html

--- hn_daily_digest.py
+++ hn_daily_digest.py
@@ -1,13 +1,13 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import requests                 # download html
-from bs4 import BeautifulSoup   # get data from html
+import requests  # download html
+from bs4 import BeautifulSoup  # get data from html
 
 links = []
 subtext = []
-for page_no in range(1,3):
-    res = requests.get('https://news.ycombinator.com/news?p='+ str(page_no))
+for page_no in range(1, 3):
+    res = requests.get('https://news.ycombinator.com/news?p=' + str(page_no))
     soup = BeautifulSoup(res.text, 'html.parser')
     links.append(soup.select('.titlelink'))
     subtext.append(soup.select('.subtext'))
@@ -19,26 +19,31 @@ def custom_hn(links, subtext):
         href = link.get('href', None)
         vote = subtext[0][n].select('.score')
         if len(vote):
-            points = int(vote[0].getText().replace(' points',''))
+            points = int(vote[0].getText().replace(' points', ''))
             if points >= 100:
-                hn.append([points,title,href])
+                hn.append([points, title, href])
     return hn
 
 def sort_by_points(hnlist):
-    return sorted(hnlist, reverse= True)
+    return sorted(hnlist, reverse=True)
 
 def save_html(content):
     data = ""
     for l in content:
         data += "<td>" + str(l[0]) + "</td>"
-        data += "<td>" + '<a href=+' + str(l[2]) + '>' + str(l[1]) + '</a>' + "</td>"
+        if l[2][:4] == 'item':
+            data += "<td>" + '<a href=https://news.ycombinator.com/' + str(
+                l[2]) + '>' + str(l[1]) + '</a>' + "</td>"
+        else:
+            data += "<td>" + '<a href=' + str(l[2]) + '>' + str(
+                l[1]) + '</a>' + "</td>"
         data += "<tr>"
     data = "<table border=1>" + data + "<table>"
 
     with open("file.html", "w") as file:
         file.write(data)
 
-def email_daily():
+def send_email():
     html_file = open("file.html", "r")
     content = html_file.read()
 
@@ -52,13 +57,13 @@ def email_daily():
     with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
         smtp.ehlo()
         smtp.starttls()
-        smtp.login('ktyntang@gmail.com','app_password')
+        smtp.login('ktyntang@gmail.com', 'app_password')        # replace with your app password
         smtp.send_message(email)
         print('sent!')
 
 def app():
     save_html((sort_by_points(custom_hn(links, subtext))))
-    email_daily()
+    send_email()
 
 if __name__ == '__main__':
     app()
